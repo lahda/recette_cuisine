93b38f33b2facae7238cfdeb84c9fbc2770d19d2
lahda <alphonsinelahda@gmail.com>
2022-09-27 17:11:39 +0200

    image recette

--- src/components/Salades.jsx
+++ src/components/Salades.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import data from "../utils/data.js"
+import styled from "styled-components"
 
 function Salades(){
 
@@ -17,6 +18,7 @@ function Salades(){
         axios.get(url).then(res => {
             console.log(res.data);
             setSalades(res.data.recipes);
+            console.log(data.recipes);
         }).catch(error => {
             console.log(error);
         });
@@ -28,13 +30,29 @@ function Salades(){
             {
                 salades.map((recipe) => {
                     return (
-                        <div key={recipe.id}>
-                            <p>{recipe.title}</p>
-                        </div>
+                        <Wrapper>
+                            <h3>salades populaires</h3>
+                            {salades.map((recipe) =>{
+                                return(
+                                    <Card>
+                                        <p>{recipe.title}</p>
+                                        <img src={recipe.image} alt={recipe.title}/>
+                                    </Card>
+                                );
+                            })}
+                        </Wrapper>
+                        
                     );
                 })
             }
         </div>
     );   
 }
+const Wrapper = styled.div`
+margin : 4rem 0rem;
+`;
+const Card = styled.div
+ ` min-height: 25rem;
+   border-radius: 5rem; 
+ `;
 export default Salades;
