0821feacd31cc7221bd6a002269e0bed689ec234
lahda <alphonsinelahda@gmail.com>
2022-10-05 13:57:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+// import {BrowserRouter, Route, Routes} from "react-router-dom";
+// import { AnimatePresence } from "framer-motion";
 
 import Home from "./pages/Home";
-import Categories from "./components/Categories";
-import Cuisine from "./pages/Cuisine";
-import Search from "./components/Search";
-import Searched from "./pages/Searched";
-import Recipe from "./pages/Recipe";
+// import Categories from "./components/Categories";
+// import Cuisine from "./pages/Cuisine";
+// import Search from "./components/Search";
+// import Searched from "./pages/Searched";
+// import Recipe from "./pages/Recipe";
 
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <AnimatePresence/>
-        <Search/>
-        <Categories/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="categories" element={<Categories/>}/>
-          <Route path="cuisine/:type" element={<Cuisine/>}/>
-          <Route path="Search" element={<Search/>}/>
-          <Route path="Searched" element={<Searched/>}/>
-          <Route path="Recipe/:name" element = {<Recipe/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
