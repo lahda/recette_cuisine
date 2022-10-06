693843be57e74ea9f25d0c8312bae78b0cbfd9d1
lahda <alphonsinelahda@gmail.com>
2022-10-06 19:01:39 +0200

    w

--- src/utils/data.js
+++ src/utils/data.js
@@ -6,7 +6,7 @@ const data = {
         research: "recipes/complexSearch",
      
     },
-    spoonacularKey: "?apiKey=c138adb653704d5ba24063308fc76c8f"
+    spoonacularKey: "?apiKey=54d2bc32e88b4e5aa736dd713196d017"
 };
 
 export default data;
